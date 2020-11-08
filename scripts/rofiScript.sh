a9902d0ad51d646838f567a44de9b21427bc1023
Dionisis Nikolopoulos <dionisis.nikolopoulos@gugroup.com>
2020-11-08 15:59:29 +0200

    The rofi is now very nice

--- scripts/rofiScript.sh
+++ scripts/rofiScript.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rofi -show drun -modi drun -hide-scrollbar -width 20 -location 1 -show-icons -font "Scientifica 8" -matching fuzzy -drun-display-format {name}
+rofi -show drun -modi drun -hide-scrollbar -width 20 -location 3 -show-icons -font "Scientifica 8" -matching fuzzy -drun-display-format {name}
